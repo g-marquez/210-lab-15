226505c8c1b7023a4f359a73fed0d4ddccc435b1
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-03-01 12:57:39 -0800

    Initial commit: starting lab 15

---
+++ 210-lab-15.cpp
@@ -0,0 +1,10 @@
+//*****************************************************************************
+// COMSC-210 | Lab 15 | Gabriel Marquez
+// Description: this program exhibits a Movie class that has the screen writer,
+// the year released, and the title as its private member variables.
+//*****************************************************************************
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
